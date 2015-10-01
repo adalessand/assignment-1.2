2bf09c3e7d7f49aa91a77b40ce8bc6f6dafd05fa
nitijsingh <nitijs@gmail.com>
2015-10-01 17:15:38 +0200

    Spacing within the commands

--- collaborative assignment1.R
+++ collaborative assignment1.R
@@ -5,13 +5,21 @@
 ##################################################################################
 # Dataset being used: women
 data("women")
+
 # to find out the variables in the dataset
-names(women)  #gives me the height and the weights 
-?women # provides me with the format of the dataset being: A data frame with 15 observations on 2 variables.
+names(women)
+
+#gives me the height and the weights 
+?women 
+
+# provides me with the format of the dataset being: A data frame with 15 observations on 2 variables.
 [,1]	height	numeric	Height (in)
 [,2]	weight	numeric	Weight (lbs)
-summary(women) # proivding me with descriptive statistics on both vaariable with, mean, median, min-max of the variables
-require(graphics) # to plot the correlation between height and weight which in this case is positive and linear
+
+summary(women)
+# proivding me with descriptive statistics on both vaariable with, mean, median, min-max of the variables
+require(graphics) 
+# to plot the correlation between height and weight which in this case is positive and linear
 plot(women, xlab = "Height (in)", ylab = "Weight (lb)",
      main = "women data: American women aged 30-39")
 summary(women$weight) #to take a closer look at the single variable 
